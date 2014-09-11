e41f538499ce4f542e8402a3b01a06eee01e309b
alexanderbsig <alexnaderbs.js@gmail.com>
2014-09-11 15:17:03 +0000

    Added functions for activities.

--- app/src/main/java/is/ru/blowfree/app/MainActivity.java
+++ app/src/main/java/is/ru/blowfree/app/MainActivity.java
@@ -1,13 +1,17 @@
 package is.ru.blowfree.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public String EXTRA_MESSAGE = "level 1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +36,19 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void newGame(View view){
+        Intent myIntent = new Intent(this, LevelActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void continueGame(View view){
+        Intent myIntent = new Intent(this, GameInstances.class);
+        startActivity(myIntent);
+    }
+    public void gameSettings(View view){
+        Intent myIntent = new Intent(this, Settings.class);
+        startActivity(myIntent);
     }
 }
