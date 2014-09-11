7a5221b7cadbdebcd49627e5c7bf3290692b7833
alexanderbsig <alexnaderbs.js@gmail.com>
2014-09-11 15:17:33 +0000

    new game activity, basic.

---
+++ app/src/main/java/is/ru/blowfree/app/NewGame.java
@@ -0,0 +1,19 @@
+package is.ru.blowfree.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by oen on 11.9.2014.
+ */
+public class NewGame extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+        Intent intent = getIntent();
+    }
+
+
+}
