c36a332b38b1db048e09cb7490bd7d86cdf68626
alexanderbsig <alexnaderbs.js@gmail.com>
2014-09-11 15:15:52 +0000

    The Game activity, basic.

---
+++ app/src/main/java/is/ru/blowfree/app/GameActivity.java
@@ -0,0 +1,20 @@
+package is.ru.blowfree.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by oen on 11.9.2014.
+ */
+public class GameActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Intent intent = getIntent();
+    }
+
+
+
+}
